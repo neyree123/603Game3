adb65ff247d545567c79c6ddb1dd58bc89d8f47c
Jackson <jvs8230@rit.edu>
2022-02-10 15:12:36 -0500

    Merged the level managers

--- 603Game3/Assets/Scripts/LevelManager.cs
+++ 603Game3/Assets/Scripts/LevelManager.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Text;
 
 public class LevelManager : MonoBehaviour
 {
     public Bubble[,] bubbleArray;
+    public int[,] colorArray;
 
     public GameObject bubblePrefab;
 
@@ -14,13 +18,15 @@ public class LevelManager : MonoBehaviour
     public Vector3 ballDisplacement;
 
     private Color[] colors;
+    private string filePath;
+    private string pathEnd = "/save.dat";
+    private bool loaded;
 
     // Start is called before the first frame update
     void Start()
     {
         // set up the colors
         colors = new Color[4];
-
 
         colors[0] = new Color(1, 0, 244.0f/255);
         colors[1] = new Color(0, 195.0f/255, 1);
@@ -31,8 +37,22 @@ public class LevelManager : MonoBehaviour
         // set up the bubble array
         bubbleArray = new Bubble[width, height];
 
+
+        // set up the bubble array
+        FillArray();
+
+    }
+
+    private void FillArray()
+    {
         float deltaX = 0.47f;
         float deltaY = 0.4f;
+
+        if (!loaded)
+        {
+            colorArray = new int[width, height];
+        }
+
         for (int i = 0; i < width; i++)
         {
             for (int j = 0; j < height; j++)
@@ -40,13 +60,63 @@ public class LevelManager : MonoBehaviour
                 GameObject bubble = Instantiate(bubblePrefab);
 
                 bubble.transform.position = new Vector3(deltaX * i + ((j % 2 == 0) ? 0 : deltaX / 2), -deltaY * j, 0) + ballDisplacement;
-                int colIdx = Random.Range(0, 4);
-                bubble.GetComponent<SpriteRenderer>().color = colors[colIdx];
                 bubble.transform.parent = transform;
 
                 bubbleArray[i, j] = bubble.GetComponent<Bubble>();
+
+                int colIdx = loaded ? colorArray[i, j] : Random.Range(0, 4);
+                colorArray[i, j] = colIdx;
+
+                bubble.GetComponent<SpriteRenderer>().color = colors[colIdx];
                 bubbleArray[i, j].setup(i, j, colIdx, GetComponent<LevelManager>());
+
             }
+        }
+    }
+
+    // Update is called once per frame
+    public void Save()
+    {
+        using (var stream = File.Open(filePath, FileMode.Create))
+        {
+            using (var writer = new BinaryWriter(stream, Encoding.Unicode, false))
+            {
+                for (int i = 0; i < colorArray.GetLength(0); i++)
+                {
+                    for (int j = 0; j < colorArray.GetLength(1); j++)
+                    {
+                        writer.Write(colorArray[i, j]);
+                    }
+                }
+            }
+        }
+    }
+
+
+    public void Load()
+    {
+        if (File.Exists(filePath))
+        {
+            foreach (Transform child in transform)
+            {
+                Destroy(child.gameObject);
+            }
+            using (var stream = File.Open(filePath, FileMode.Open))
+            {
+                using (var reader = new BinaryReader(stream, Encoding.UTF8, false))
+                {
+                    colorArray = new int[width, height];
+                    for (int i = 0; i < width; i++)
+                    {
+                        for (int j = 0; j < height; j++)
+                        {
+                            colorArray[i, j] = reader.ReadInt32();
+                        }
+                    }
+                }
+            }
+            loaded = true;
+            FillArray();
         }
     }
 
@@ -109,11 +179,6 @@ public class LevelManager : MonoBehaviour
         }
     }
 
-    private void tryPop(int color, Bubble bubble)
-    {
-        if (bubble.color == color)
-            bubble.Pop();
-    }
 
     // Update is called once per frame
     void Update()
@@ -121,3 +186,4 @@ public class LevelManager : MonoBehaviour
         
     }
 }
+
