5b690108421c3623144285ff5ff62ac3e31e50ee
Destiny Treptau <treptaudestiny@gmail.com>
2022-02-11 00:19:04 -0500

    Health UI

--- 603Game3/Assets/Scripts/FurbyController.cs
+++ 603Game3/Assets/Scripts/FurbyController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class FurbyController : MonoBehaviour
@@ -12,7 +13,16 @@ public class FurbyController : MonoBehaviour
     public float maxXSpeed;
     public float xLerp;
 
-    public int health = 10;
+    public float maxHealth = 10;
+    public float health;
+
+    public GameObject healthWaves;
+    public float yBounds = 9;
+    public float healthUISpeed;
+    private float startY;
+    private float targetY = .1f;
+
+    public Image healthBar;
 
 
     // Start is called before the first frame update
@@ -21,6 +31,13 @@ public class FurbyController : MonoBehaviour
         tail = GetComponent<follow>();
         rb = GetComponent<Rigidbody2D>();
         rb.velocity = new Vector2(0, ySpeed); // launch upwards
+
+        health = maxHealth;
+
+        startY = healthWaves.transform.position.y;
+        targetY = startY;
+        //healthBar.fillMethod = Image.FillMethod.Horizontal;
+        //healthBar.fillOrigin = (int)Image.OriginHorizontal.Left;
     }
 
     // Update is called once per frame
@@ -33,6 +50,17 @@ public class FurbyController : MonoBehaviour
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
         }
 
+        targetY = startY - ((maxHealth - health) * (yBounds / maxHealth));
+
+        Debug.Log(targetY);
+
+        if (healthWaves.transform.position.y > targetY)
+        {
+            healthWaves.transform.position -= new Vector3(0, healthUISpeed, 0);
+            healthBar.fillAmount = Mathf.Clamp(health / maxHealth, 0, 1f);
+        }
+
+        //healthWaves.transform.position = new Vector3(healthWaves.transform.position.x, startY - ((maxHealth - health) * (yBounds / maxHealth)));
 
         float xDir = 0f;
         if (Input.GetKey(KeyCode.A))
@@ -49,6 +77,11 @@ public class FurbyController : MonoBehaviour
         rb.velocity = new Vector2(xVel, ySpeed);
 
         transform.up = rb.velocity;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            health--;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
