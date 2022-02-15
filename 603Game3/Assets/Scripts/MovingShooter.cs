b6a8d5038469c90e341ebd0bd040836cc28c42d9
Destiny Treptau <treptaudestiny@gmail.com>
2022-02-15 14:52:04 -0500

    Temp

--- 603Game3/Assets/Scripts/MovingShooter.cs
+++ 603Game3/Assets/Scripts/MovingShooter.cs
@@ -16,14 +16,25 @@ public class MovingShooter : MonoBehaviour
     public GameObject bullet;
     private Transform bulletHolder;
 
+    public GameObject laserPrefab;
+    private GameObject laser;
+    public float laserTime;
+
     public GameObject chargeBarParent;
     public GameObject chargeBar;
 
     private Vector3 barLocalScale;
 
+    private bool laserFiring;
+    public float laserMaxYScale = 10;
+    public float laserScaleFactor = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
+        //laser = transform.GetChild(1).gameObject;
+        //laser.SetActive(false);
+
         bulletHolder = GameObject.Find("BulletHolder").transform;
 
 
@@ -82,22 +93,43 @@ public class MovingShooter : MonoBehaviour
                     chargeBar.transform.localScale = barLocalScale;
                 }
 
-
             }
 
             if (Input.GetKeyUp(KeyCode.W) && attackTimer > timeBetweenBullets)
             {
-                GameObject b = Instantiate(bullet, transform.position, Quaternion.identity, bulletHolder);
+                //GameObject b = Instantiate(bullet, transform.position, Quaternion.identity, bulletHolder);
 
                 if (chargeTimer >= chargeTime)
                 {
-                    b.GetComponent<BulletScript>().isCharged = true;
+                    //b.GetComponent<BulletScript>().isCharged = true;
+                    GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity, bulletHolder);
+                    StartCoroutine(Laser());
+                }
+                else
+                {
+                    GameObject b = Instantiate(bullet, transform.position, Quaternion.identity, bulletHolder);
                 }
 
                 chargeBarParent.SetActive(false);
                 chargeTimer = 0;
                 attackTimer = 0;
             }
+
+
+            if (laserFiring && laser.transform.localScale.y < laserMaxYScale)
+            {
+                laser.transform.localScale += new Vector3(0, laserScaleFactor, 0);
+            }
         }
+    }
+
+    public IEnumerator Laser()
+    {
+        //laser.SetActive(true);
+        laserFiring = true;
+        laser.transform.localScale = new Vector3(laser.transform.localScale.x, .1f, laser.transform.localScale.z);
+        yield return new WaitForSeconds(laserTime);
+        laserFiring = false;
+        //laser.SetActive(false);
     }
 }
